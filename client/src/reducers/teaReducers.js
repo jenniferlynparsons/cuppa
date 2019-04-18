92446afd7a19bd4e2b335c7a8f70af3a7df3adae
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-04-18 15:37:39 -0400

    delete normalization cleanup

--- client/src/reducers/teaReducers.js
+++ client/src/reducers/teaReducers.js
@@ -3,13 +3,11 @@ export default (state = {}, action) => {
     case "ADD_TEA":
     case "EDIT_TEA":
       return { ...state, ...{ [action.payload.id]: action.payload } };
+
     case "DELETE_TEA":
-      return Object.keys(state).reduce((result, key) => {
-        if (key !== action.payload.id) {
-          result[key] = state[key];
-        }
-        return result;
-      }, {});
+      const { [action.payload.id]: val, ...newState } = state;
+      return newState;
+
     case "GET_TEAS":
       return { ...state, ...action.payload };
   }
