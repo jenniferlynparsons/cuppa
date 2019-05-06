51f8d33d9eab14f1d3223de17de9978267064f36
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-05-06 10:44:38 -0400

    filter input setup

--- client/src/components/Tea/TeaList/TableFilter.jsx
+++ client/src/components/Tea/TeaList/TableFilter.jsx
@@ -4,24 +4,31 @@ export class TableFilter extends React.Component {
   render() {
     return (
       <div className="columns is-pulled-right">
-        <div className="field column">
-          <label className="label">Filter By</label>
-          <div className="control">
-            <div className="select">
+        <div className="field has-addons">
+          <p className="control">
+            <span className="select is-small">
               <select>
                 {this.props.columnHeaders.map(colHeaderObj => {
                   return <option>{colHeaderObj.colTitle}</option>;
                 })}
               </select>
-            </div>
-          </div>
+            </span>
+          </p>
+          <p className="control">
+            {/* this should do autocomplete similar to tea brands */}
+            <input
+              className="input is-small"
+              type="text"
+              placeholder="Filter Text"
+            />
+          </p>
+          <p className="control">
+            <a className="button is-small">Filter</a>
+          </p>
         </div>
-
-        <div className="field column">
-          <label className="label">Filter</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Text input" />
-          </div>
+        {/* should only be visible when filters are applied */}
+        <div className="control">
+          <button className="button is-primary is-small">Clear Filter</button>
         </div>
       </div>
     );
