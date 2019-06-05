aa15c8cba1cabfc274dfc30bb10e79cf66e7d605
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-06-05 18:06:39 -0400

    Feature/action tests (#18)
    * initial action tests
    
    * Set up authActions to allow spying on individual functions
    
    * authAction tests
    
    * action tests full coverage
    
    * fixes based on review

---
+++ client/src/actions/__tests__/authActions.test.js
@@ -0,0 +1,145 @@
+import { makeMockStore } from "../../test/testUtils";
+import authActions from "../authActions";
+import jwt_decode from "jwt-decode";
+import API from "../../lib/api";
+import setAuthToken from "../../utils/setAuthToken";
+
+const store = makeMockStore({
+  auth: {
+    isAuthenticated: false,
+    user: {},
+    loading: false
+  }
+});
+
+const mockLoginData = {
+  email: "jenniferlynparsons@gmail.com",
+  password: "boogadoo1"
+};
+
+const mockRegisterData = {
+  name: "Jennifer",
+  email: "littleoracle@gmail.com",
+  password: "boogadoo1",
+  password2: "boogadoo1"
+};
+
+const mockHistory = {
+  length: 10,
+  action: "PUSH",
+  location: { pathname: "/dashboard", search: "", hash: "", key: "zh4boo" },
+  push: jest.fn()
+};
+
+const mockResponse = {
+  success: true,
+  token:
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjNjMxM2E0YzMxOGJiNjIyOThiMjNkNCIsIm5hbWUiOiJKZW5uaWZlciIsImlhdCI6MTU1ODk3MTY4MSwiZXhwIjoxNTkwNTI4NjA3fQ.1Tn8FLJE2gUj0D0ka91e79cLEQz6ttSkQ0sZNd3GGE8"
+};
+
+const mockDecodedToken = {
+  id: "5c6313a4c318bb62298b23d4",
+  name: "Jennifer",
+  iat: 1559234664,
+  exp: 1590791590
+};
+
+let loginAction = authActions.loginAction;
+let registerUser = authActions.registerUser;
+let logoutUser = authActions.logoutUser;
+let setCurrentUser = authActions.setCurrentUser;
+
+// Set up our stubbed auth function
+jest.mock("../../utils/setAuthToken", () => jest.fn());
+
+jest.mock("jwt-decode");
+
+// Set up our stubbed API.
+jest.mock("../../lib/api", () => ({
+  post: jest.fn(() => Promise.resolve(mockResponse))
+}));
+
+describe("loginAction", () => {
+  test("returns a function", () => {
+    expect(loginAction(mockLoginData)).toBeInstanceOf(Function);
+  });
+
+  describe("dispatching the returned function", () => {
+    test("it calls 'post' on the API with the correct path and the user data", () => {
+      store.dispatch(loginAction(mockLoginData));
+      expect(API.post).toHaveBeenCalledWith("/users/login", mockLoginData);
+    });
+
+    describe("when the POST call is successful", () => {
+      test("it sets the JWT token to the response from the POST", async () => {
+        await store.dispatch(loginAction(mockLoginData));
+        expect(setAuthToken).toHaveBeenCalledWith(mockResponse.token);
+      });
+      test("it decodes the token with jwt_decode", async () => {
+        await store.dispatch(loginAction(mockLoginData));
+        expect(jwt_decode).toHaveBeenCalledWith(mockResponse.token);
+      });
+      test("it sets the current user action", async () => {
+        let currentUserSpy = jest.spyOn(authActions, "setCurrentUser");
+        await store.dispatch(authActions.loginAction(mockLoginData));
+        expect(currentUserSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
+
+describe("registerUser", () => {
+  test("returns a function", () => {
+    expect(registerUser(mockRegisterData)).toBeInstanceOf(Function);
+  });
+
+  describe("dispatching the returned function", () => {
+    test("it calls 'post' on the API with the correct path and the user data", () => {
+      store.dispatch(registerUser(mockRegisterData, mockHistory));
+      expect(API.post).toHaveBeenCalledWith(
+        "/users/register",
+        mockRegisterData
+      );
+    });
+
+    describe("when the POST call is successful", () => {
+      test("it sets the JWT token to the response from the POST", async () => {
+        await store.dispatch(registerUser(mockRegisterData, mockHistory));
+        expect(setAuthToken).toHaveBeenCalledWith(mockResponse.token);
+      });
+      test("it decodes the token with jwt_decode", async () => {
+        await store.dispatch(registerUser(mockRegisterData, mockHistory));
+        expect(jwt_decode).toHaveBeenCalledWith(mockResponse.token);
+      });
+      test("it sets the current user action", async () => {
+        let currentUserSpy = jest.spyOn(authActions, "setCurrentUser");
+        await store.dispatch(
+          authActions.registerUser(mockRegisterData, mockHistory)
+        );
+        expect(currentUserSpy).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
+
+describe("logoutUser", () => {
+  test("returns a function", () => {
+    expect(logoutUser()).toBeInstanceOf(Function);
+  });
+  test("it resets the authToken", async () => {
+    await store.dispatch(logoutUser());
+    expect(setAuthToken).toHaveBeenCalled();
+  });
+});
+
+describe("setCurrentUser", () => {
+  test("returns an object", () => {
+    expect(setCurrentUser(mockDecodedToken)).toBeInstanceOf(Object);
+  });
+  test("it returns the SET_CURRENT_USER action type and decoded payload", () => {
+    expect(setCurrentUser(mockDecodedToken)).toEqual({
+      type: "SET_CURRENT_USER",
+      payload: mockDecodedToken
+    });
+  });
+});
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,32 +4,33 @@ import API from "../lib/api";
 import { authActionTypes } from "../lib/actionTypes";
 
 // Login - get user token
-export function loginAction(userData) {
+export const loginAction = userData => {
   return dispatch => {
-    API.post(`/users/login`, userData).then(response => {
+    API.post("/users/login", userData).then(response => {
       const { token } = response;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
       const decoded = jwt_decode(token);
-      dispatch(setCurrentUser(decoded));
+      //check that setCurrentUser is called with decoded
+      dispatch(authActions.setCurrentUser(decoded));
     });
   };
-}
+};
 
 // Register User
-export function registerUser(userData, history) {
+export const registerUser = (userData, history) => {
   return dispatch => {
-    API.post(`/users/register`, userData).then(response => {
+    API.post("/users/register", userData).then(response => {
       const arg = "/login";
       history.push(arg);
       const { token } = response;
       localStorage.setItem("jwtToken", token);
       setAuthToken(token);
       const decoded = jwt_decode(token);
-      dispatch(setCurrentUser(decoded));
+      dispatch(authActions.setCurrentUser(decoded));
     });
   };
-}
+};
 
 // Log user out
 export const logoutUser = () => dispatch => {
@@ -44,3 +45,12 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+const authActions = {
+  loginAction,
+  registerUser,
+  logoutUser,
+  setCurrentUser
+};
+
+export default authActions;
