bed52bf649f6dc45a0ec202550f7b13b24c1f22a
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-06-05 18:06:57 -0400

    reducer test setup (#17)

---
+++ client/src/reducers/__tests__/authReducers.test.js
@@ -0,0 +1,59 @@
+import { authTypes } from "../../lib/actionTypes";
+import authReducer from "../authReducers";
+
+const initialState = {
+  isAuthenticated: false,
+  user: {},
+  loading: false
+};
+
+const userLoadingPayload = {
+  loading: true
+};
+
+const userLoadingState = {
+  isAuthenticated: false,
+  user: {},
+  loading: true
+};
+
+const userLoggedInPayload = {
+  exp: 1590017571,
+  iat: 1558460645,
+  id: "5c6313a4c318bb62298b23d4",
+  name: "Jennifer"
+};
+
+const expectedLoggedInState = {
+  isAuthenticated: true,
+  loading: false,
+  user: {
+    exp: 1590017571,
+    iat: 1558460645,
+    id: "5c6313a4c318bb62298b23d4",
+    name: "Jennifer"
+  }
+};
+
+describe("auth reducer", () => {
+  test("returns default state when there is no action type", () => {
+    const reducer = authReducer(initialState, {});
+    expect(reducer).toEqual(initialState);
+  });
+
+  test("returns loading state true when the action type is 'USER_LOADING'", () => {
+    const reducer = authReducer(initialState, {
+      type: "USER_LOADING",
+      payload: userLoadingPayload
+    });
+    expect(reducer).toEqual(userLoadingState);
+  });
+
+  test("returns logged in user state when the action type is 'SET_CURRENT_USER'", () => {
+    const reducer = authReducer(initialState, {
+      type: "SET_CURRENT_USER",
+      payload: userLoggedInPayload
+    });
+    expect(reducer).toEqual(expectedLoggedInState);
+  });
+});
---
+++ client/src/reducers/__tests__/flashReducers.test.js
@@ -0,0 +1,16 @@
+import { flashActionTypes } from "../../lib/actionTypes";
+import flashReducer from "../flashReducers";
+
+describe("flash reducer", () => {
+  test("returns default state when there is no action type", () => {
+    const reducer = flashReducer(undefined, {});
+    expect(reducer).toEqual("off");
+  });
+  test("returns 'on' state when action.payload is 'on'", () => {
+    const reducer = flashReducer(undefined, {
+      type: "EDIT_TEA_FLASH",
+      payload: "on"
+    });
+    expect(reducer).toEqual("on");
+  });
+});
