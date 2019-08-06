b92d08d24cbe328d8750c5539f3584c30b3c61aa
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-08-06 08:49:48 -0400

    Revert "moved redux rendered tests to a separate ticket"
    This reverts commit 734fc9b12e7f730f71bb7173748b66300e0eb264.

--- client/src/components/TeaTypes/TeaTypeEditor/__tests__/TeaTypeEditorContainer.test.jsx
+++ client/src/components/TeaTypes/TeaTypeEditor/__tests__/TeaTypeEditorContainer.test.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "jest-dom/extend-expect";
 import { fireEvent } from "@testing-library/react";
 import { renderWithRouter } from "../../../../test/routerTestUtils";
+import { makeMockStore } from "../../../../test/testUtils";
 import dataFixture from "../../../../test/__fixtures__/dataFixture";
 import storeFixture from "../../../../test/__fixtures__/storeFixture";
 import teaTypeFixture from "../../../../test/__fixtures__/teaTypeFixture";
+import TeaTypeEditorContainer from "../TeaTypeEditorContainer";
 import { TeaTypeEditorContainerClass } from "../TeaTypeEditorContainer";
 
 let mockFunc;
@@ -15,6 +17,23 @@ beforeEach(() => {
   mockFunc = jest.fn(() => Promise.resolve(storeFixture.basicStore));
   mockAdd = jest.fn(() => Promise.resolve(storeFixture.addedStore));
   mockEdit = jest.fn(() => Promise.resolve(storeFixture.updatedStore));
+});
+
+describe("TeaTypeEditorContainer rendering", () => {
+  test("renders the component with redux without errors", async () => {
+    let store = makeMockStore(storeFixture.basicStore);
+    const { queryByTestId } = renderWithRouter(
+      <TeaTypeEditorContainer
+        store={store}
+        match={{
+          params: { id: dataFixture.mockUserID }
+        }}
+      />
+    );
+    await Promise.resolve();
+
+    expect(queryByTestId("teatypeeditor")).toBeTruthy();
+  });
 });
 
 describe("teaTypeEditor form success", () => {
