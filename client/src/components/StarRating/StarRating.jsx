42054a7d18e57aeda3cd6ffec7bc615bf14349f9
Jennifer Lyn Parsons <jenniferlynparsons@gmail.com>
2019-10-10 19:09:00 -0400

    create star rating fragment

--- client/src/components/StarRating/StarRating.jsx
+++ client/src/components/StarRating/StarRating.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faStar);
+import StarSpan from "./StarSpan";
 
 class StarRating extends React.Component {
   stars = [
@@ -22,20 +18,8 @@ class StarRating extends React.Component {
           {this.stars.map(star => {
             return (
               <label htmlFor={star.name} key={star.name}>
-                <span className="is-hidden" aria-hidden="false">
-                  {star.name}
-                </span>
-                <span className="icon" aria-hidden="true">
-                  <i className="fas">
-                    <FontAwesomeIcon
-                      icon="star"
-                      className={[
-                        "star",
-                        this.props.rating >= star.value ? "active" : ""
-                      ].join(" ")}
-                    />
-                  </i>
-                </span>
+                <StarSpan rating={this.props.rating} star={star} />
+
                 <input
                   data-testid={"star" + star.name}
                   id={star.name}
@@ -57,20 +41,7 @@ class StarRating extends React.Component {
           {this.stars.map(star => {
             return (
               <div key={star.name} className="is-inline">
-                <span className="is-hidden" aria-hidden="false">
-                  {star.name}
-                </span>
-                <span className="icon" aria-hidden="true">
-                  <i className="fas">
-                    <FontAwesomeIcon
-                      icon="star"
-                      className={[
-                        "star",
-                        this.props.rating >= star.value ? "active" : ""
-                      ].join(" ")}
-                    />
-                  </i>
-                </span>
+                <StarSpan rating={this.props.rating} star={star} />
               </div>
             );
           })}
